949426c292b1d40310af3fe3b3cb0be9d4862228
radrovi735-hue <radrovi735@gmail.com>
2026-09-10 15:16:50 -0300

    gameplay: register canonical mission and catechist routes

--- routes/crismaquest.php
+++ routes/crismaquest.php
@@ -12,3 +12,18 @@ $router->post('/studenti/correio/troca/{id}/aceitar', [CrismaQuestSocialControll
 $router->post('/studenti/correio/troca/{id}/recusar', [CrismaQuestSocialController::class, 'declineTrade']);
 $router->post('/studenti/correio/cosmetico/{id}/usar', [CrismaQuestSocialController::class, 'equipCosmetic']);
 $router->get('/docenti/correio', [CrismaQuestSocialController::class, 'teacherAudit']);
+
+
+// Motor canônico do jogo CrismaQuest.
+$router->get('/studenti/missoes', [CrismaQuestGameController::class, 'studentIndex']);
+$router->post('/studenti/missoes/{id}/concluir', [CrismaQuestGameController::class, 'completeMission']);
+$router->post('/studenti/centelha/concluir', [CrismaQuestGameController::class, 'completeSpark']);
+$router->post('/studenti/baus/{id}/abrir', [CrismaQuestGameController::class, 'claimChest']);
+$router->post('/studenti/chama/intercessao', [CrismaQuestGameController::class, 'sendIntercession']);
+$router->post('/studenti/chama/intercessao/{id}/usar', [CrismaQuestGameController::class, 'useIntercession']);
+$router->post('/studenti/chama/rosario', [CrismaQuestGameController::class, 'useRosary']);
+
+$router->get('/docenti/jogo', [CrismaQuestGameController::class, 'teacherIndex']);
+$router->post('/docenti/jogo/missao/{id}/toggle', [CrismaQuestGameController::class, 'toggleMission']);
+$router->post('/docenti/jogo/missao/{id}/duplicar', [CrismaQuestGameController::class, 'duplicateMission']);
+$router->post('/docenti/jogo/pausa', [CrismaQuestGameController::class, 'pauseClass']);
